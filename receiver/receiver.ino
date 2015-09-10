01f8f5cfbd9bd09ab7fc5b4f860fd5be95088594
uminntyu <tkmkaito@gmail.com>
2015-09-10 16:56:52 +0900

    first commit

---
+++ receiver/receiver.ino
@@ -0,0 +1,15 @@
+/*
+ Name:		receiver.ino
+ Created:	2015/09/10 16:54:15
+ Author:	uminntyu
+*/
+
+// the setup function runs once when you press reset or power the board
+void setup() {
+
+}
+
+// the loop function runs over and over again until power down or reset
+void loop() {
+  
+}
